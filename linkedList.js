e9c05c696e72a92f7622df3fc8c70903c52da9d1
gefgu <gefgu@hotmail.com>
2022-08-25 14:44:43 -0300

    Add initial setup

---
+++ linkedList.js
@@ -0,0 +1,9 @@
+function Node(value = null, next = null) {
+  return { value, next };
+}
+
+function LinkedList(root) {
+  return { root };
+}
+
+export { Node, LinkedList };
---
+++ linkedList.test.js
@@ -0,0 +1,5 @@
+import { Node, LinkedList } from "./linkedList";
+
+test("Factories working", () => {
+  expect(1).toBe(1);
+});
